de125f6ddb8e48569f5c416ee65176515bb55b46
Gianluca Pozzo <gianluca.pozzo@sva.de>
2023-09-21 13:34:31 +0200

    Fixed typo

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@ use dotenv::dotenv;
 use postgres::{Client, Error, NoTls};
 use std::{env, time::Duration};
 
-
-
 fn main() -> Result<(), Error> {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("ERROR: Expected \"DB_URL\"");
 
-    let mut client = conncet_to_database(&db_url)?;
+    let mut client = connect_to_database(&db_url)?;
 
     if client.is_valid(Duration::new(5, 0)).is_ok() {
         println!("Connection to DB successfull!")
@@ -17,7 +15,7 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn conncet_to_database(url: &String) -> Result<Client, Error> {
+fn connect_to_database(url: &String) -> Result<Client, Error> {
     let client = Client::connect(url, NoTls)?;
     Ok(client)
 }
