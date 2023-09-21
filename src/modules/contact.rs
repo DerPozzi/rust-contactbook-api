ddf00283ea74dc7001b6e3c0a84198f309557988
Gianluca Pozzo <gianluca.pozzo@sva.de>
2023-09-21 14:21:18 +0200

    Updated contact struct

--- src/modules/contact.rs
+++ src/modules/contact.rs
@@ -1,18 +1,20 @@
+#[derive(Debug)]
 pub struct Contact {
-    name: String,
-    birthday: Option<String>,
-    phone: Option<String>,
-    email: Option<String>,
-    notes: Option<String>,
+    pub id: Option<i32>,
+    pub name: String,
+    pub birthday: Option<String>,
+    pub phone: Option<String>,
+    pub email: Option<String>,
+    pub notes: Option<String>,
 }
 
-impl Contact {
-    pub fn new(
-        name: String,
-        birthday: String,
-        phone: String,
-        email: String,
-        notes: String,
-    ) -> Self {
-    }
-}
+// impl Contact {
+//     pub fn new(
+//         name: String,
+//         birthday: String,
+//         phone: String,
+//         email: String,
+//         notes: String,
+//     ) -> Self {
+//     }
+// }
